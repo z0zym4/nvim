4a9a6eefdad9876b5907498cda17987aa8f0dbef
z0zym4 <z0zym4@proton.me>
2024-03-05 21:10:38 -0500

    Remove playground

--- lua/z0zym4/packer.lua
+++ lua/z0zym4/packer.lua
@@ -23,6 +23,5 @@ return require("packer").startup(function(use)
     })
 
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-    use("nvim-treesitter/playground")
     use("theprimeagen/harpoon")
 end)
